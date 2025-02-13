d492ccc529711483f6872eeaa155b552803f7c2e
Gabe Kotton <52292616+FlamingoeZpZ@users.noreply.github.com>
2025-02-13 18:35:56 -0500

    Looking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,9 @@
 using System;
 using Scriptable_Objects;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
+
 [SelectionBase]
 public class PlayerController : MonoBehaviour
 {
@@ -12,13 +15,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float footDist;
     [SerializeField] private float footRadius;
     [SerializeField] private Transform foot;
+    
+    [Header("Animation")]
+    [SerializeField] private Transform headProxy;
+    [SerializeField] private Transform headBone;
+    [SerializeField, Min(0)] private float rotationAnimationSpeed;
+    [SerializeField, Min(0)] private float animationReturnSpeed;
+
+    [SerializeField, Range(-1,1)] private float rotationAnimationThreshold;
+
+    
     [Header("Looking")] 
-    [SerializeField] private Transform lookPivot;
-    [SerializeField] private Transform theRealHead;
-    [SerializeField] private Transform head;
-    [SerializeField] private Transform body;
-    [SerializeField] private float rotationThreshold;
-    [SerializeField] private float pitchLimit;
+    [SerializeField] private Transform headXRotator;
+    [SerializeField] private Transform bodyYRotator;
+    [SerializeField, Min(0)] private float rotationSpeed;
+    [SerializeField, Range(0,90)] private float pitchLimit;
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -31,6 +42,8 @@ public class PlayerController : MonoBehaviour
 
     private void LateUpdate()
     {
+        HandleLooking();
+        /*
         float dot = Vector3.Dot(head.forward, lookPivot.forward);
         if (dot > rotationThreshold)
         {
@@ -51,6 +64,38 @@ public class PlayerController : MonoBehaviour
             }
         }
         theRealHead.rotation = lookPivot.rotation;
+        */
+    }
+
+    private void HandleLooking()
+    {
+        float dt = Time.deltaTime; //Cache this for easy optimization
+        
+        //First, let's rotate around the Y axis (left and right rotation)
+        bodyYRotator.Rotate(Vector3.up, _curLookDir.x * rotationSpeed * dt);
+        
+        //Next, we want to rotate around the X axis, BUT we need to be careful to not look to high up.
+        float newXRotation = headXRotator.localEulerAngles.x;
+        
+        newXRotation = (newXRotation > 180f) ? newXRotation - 360f : newXRotation; // Convert to -180 to 180 range (Because Unity sucks)
+        
+        newXRotation = Mathf.Clamp(newXRotation + _curLookDir.y * rotationSpeed * dt, -pitchLimit, pitchLimit);
+        
+        //Set the rotation
+        headXRotator.localRotation = Quaternion.Euler(newXRotation, headXRotator.localEulerAngles.y, headXRotator.localEulerAngles.z);
+        
+       //Rotate the head in both directions...
+       headProxy.Rotate(Vector3.up, _curLookDir.x * rotationAnimationSpeed * dt);
+       headProxy.Rotate(Vector3.right, _curLookDir.y * rotationAnimationSpeed * dt);
+
+       //Check if we exceed the threshold, if we do then rotate the head slowly to correct location
+       if (Vector3.Dot(headProxy.forward, headXRotator.forward) < rotationAnimationThreshold)
+       {
+           headProxy.rotation= Quaternion.Slerp(headProxy.rotation, headXRotator.rotation, animationReturnSpeed * dt); // Use rotation speed
+       }
+       
+       headBone.rotation = headProxy.rotation;
+        
     }
 
     public void Jump(bool readValueAsButton)

c8bfa65fd442d00cc1c922369447e3b4529c7710
Eisenfyre <109298676+Eisenfyre@users.noreply.github.com>
2025-03-06 20:39:20 -0500

    door

--- Assets/Scripts/Abilities/LaserEyeGun.cs
+++ Assets/Scripts/Abilities/LaserEyeGun.cs
@@ -13,8 +13,9 @@ namespace Abilities
             Vector3 leftLaserDir = (cameraForward - _oner.laserSpawn.position).normalized;
             Vector3 rightLaserDir = (cameraForward - _oner.laserSpawn2.position).normalized;
             
-            Instantiate(laser, _oner.laserSpawn.position, Quaternion.LookRotation(leftLaserDir, Vector3.up)).Init(gameObject);
-            Instantiate(laser, _oner.laserSpawn2.position, Quaternion.LookRotation(rightLaserDir, Vector3.up)).Init(gameObject);
+            Instantiate(laser, _oner.laserSpawn.position, Quaternion.LookRotation(leftLaserDir, Vector3.up)).Init(_oner.gameObject);
+            Instantiate(laser, _oner.laserSpawn2.position, Quaternion.LookRotation(rightLaserDir, Vector3.up)).Init(_oner.gameObject);
         }
+
     }
 }
--- Assets/Scripts/Objects/Laser.cs
+++ Assets/Scripts/Objects/Laser.cs
@@ -1,6 +1,7 @@
 using System;
 using Interfaces;
 using Scriptable_Objects;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -12,13 +13,13 @@ namespace Objects
         [SerializeField]private ProjectileStats laserStats;
         [SerializeField]private new Rigidbody rigidbody;
         private GameObject _oner;
+        [SerializeField] private GameObject laserSpark;
         public void Init(GameObject oner)
         {
             _oner = oner; 
             Destroy(gameObject, 3);
             rigidbody.AddForce(transform.forward * laserStats.Speed, ForceMode.Impulse);
         }
-
         private void OnTriggerEnter(Collider other)
         {
             Rigidbody hitInfoRigidbody = other.attachedRigidbody;
@@ -32,6 +33,9 @@ namespace Objects
                     damageable.TakeDamage(laserStats.Damage, Vector3.zero);
                 }
             }
+            bool hit = Physics.Raycast(transform.position + transform.forward * -0.25f, transform.forward, out RaycastHit hitInfo, 0.5f);
+            GameObject t = Instantiate(laserSpark, hitInfo.point, Quaternion.LookRotation(hitInfo.normal), other.transform);
+            Destroy(t, 5);
             Destroy(gameObject);
         }
     }

2db9c3d2459adad2838f0bab59fc7900fcf87ee2
Eisenfyre <109298676+Eisenfyre@users.noreply.github.com>
2025-04-17 20:42:57 -0400

    push

---
+++ Assets/Scripts/Potion.cs
@@ -0,0 +1,75 @@
+using System;
+using Interfaces;
+using Scriptable_Objects;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[SelectionBase]
+public class Potion : MonoBehaviour, IDamageable, IInteractable
+{
+    private float _previousSpeed;
+    [SerializeField] private HoverInfoStats hoverInfo;
+    [SerializeField] private float breakForce = -9; // This number should be squared manually so 10 --> 100.
+    private Rigidbody _rb;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _previousSpeed = _rb.linearVelocity.sqrMagnitude;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //While using SqrMagnitude is logically faster, the difference is very minor.
+        Vector3 velocity = _rb.linearVelocity;
+        float speed = velocity.sqrMagnitude;
+            
+        //If it's negative, then we've suddenly slowed down
+        if (speed - _previousSpeed < breakForce)
+        {
+            Break(velocity);
+        }
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnHurt(float amount, Vector3 force)
+    {
+        Break(-force);
+    }
+
+    public float Health { get; set; }
+    public float DamageRes { get; }
+
+    private void Break(Vector3 direction)
+    {
+        //var c = Instantiate(cloud, transform.position, Quaternion.LookRotation(direction));
+        //c.Initialize(_effect);
+        Die();
+    }
+
+    public void OnHover()
+    {
+       Debug.Log("potion hover");
+    }
+
+    public void OnHoverEnd()
+    {
+        Debug.Log("potion hover end");
+    }
+
+    public void OnInteract()
+    {
+        Debug.Log("potion hover interact");
+    }
+
+    public HoverInfoStats GetHoverInfoStats()
+    {
+        return hoverInfo;
+    }
+}

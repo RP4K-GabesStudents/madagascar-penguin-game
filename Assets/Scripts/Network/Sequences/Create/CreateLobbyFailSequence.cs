413f46f7b27c04e8e5c20853addd14d47c22c61a
FlamingoeZpZ <gabekotton@gmail.com>
2026-05-25 22:28:29 -0400

    f

--- Assets/Scripts/Network/Sequences/Create/CreateLobbyFailSequence.cs
+++ Assets/Scripts/Network/Sequences/Create/CreateLobbyFailSequence.cs
@@ -1,13 +1,24 @@
 using System;
+using Cysharp.Threading.Tasks;
+using GabesCommonUtility.Sequence;
 using UnityEngine;
 
 namespace Network.Sequences.Create
 {
-    public class CreateLobbyFailSequence : MonoBehaviour
+    public class CreateLobbyFailSequence : MonoBehaviour, IEntrySequence
     {
         private void Awake()
         {
             Debug.LogError("Lobby failed to create");
         }
+
+        public event Action<string> DisplayMessage;
+        public UniTask<IEntrySequence> ExecuteSequence()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEntrySequence Default { get; }
+        public bool IsCompleted { get; }
     }
 }
